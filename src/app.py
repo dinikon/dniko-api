734db525051be4f29e42a7d881c9beb1ce3df0de
dNikon <denis.inikon@gmail.com>
2025-06-22 17:51:50 +0300

    Black format project

--- src/app.py
+++ src/app.py
@@ -1,4 +1,10 @@
+from importlib import reload
 
-from app_factory import create_app
+import uvicorn
+
+from src.app_factory import create_app
 
 app = create_app()
+
+if __name__ == "__main__":
+    uvicorn.run("src.app:app", host="0.0.0.0", port=8000, reload=True)
--- src/app_factory.py
+++ src/app_factory.py
@@ -1,8 +1,10 @@
 from src.dniko_app import DNikoApp
 
+
 def create_fast_api_app():
     d_niko_app = DNikoApp()
     return d_niko_app
+
 
 def create_app() -> DNikoApp:
     app = create_fast_api_app()
