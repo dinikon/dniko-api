b169ce63f6246af4473155aa2dfd11374df0bd3c
dNikon <denis.inikon@gmail.com>
2025-06-22 18:29:07 +0300

    Add config app

---
+++ src/config/__init__.py
@@ -0,0 +1,3 @@
+from config.app_config import DNikoConfig
+
+d_niko_config = DNikoConfig()
---
+++ src/config/app_config.py
@@ -0,0 +1,35 @@
+from pydantic_settings import SettingsConfigDict, BaseSettings, PydanticBaseSettingsSource
+from config.deploy import DeploymentConfig
+from config.packaging import PackagingInfo
+
+
+class DNikoConfig(
+    # Packaging info
+    PackagingInfo,
+    # Deployment configs
+    DeploymentConfig,
+):
+    model_config = SettingsConfigDict(
+        # read from dotenv format config file
+        env_file=".env",
+        env_file_encoding="utf-8",
+        # ignore extra attributes
+        extra="ignore",
+    )
+
+    @classmethod
+    def settings_customise_sources(
+            cls,
+            settings_cls: type[BaseSettings],
+            init_settings: PydanticBaseSettingsSource,
+            env_settings: PydanticBaseSettingsSource,
+            dotenv_settings: PydanticBaseSettingsSource,
+            file_secret_settings: PydanticBaseSettingsSource,
+    ) -> tuple[PydanticBaseSettingsSource, ...]:
+        return (
+            init_settings,
+            env_settings,
+            # RemoteSettingsSourceFactory(settings_cls),
+            dotenv_settings,
+            file_secret_settings,
+        )
