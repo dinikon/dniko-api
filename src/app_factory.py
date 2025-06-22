ed2d1f4fc37a8ba00fcae5945e01ab25d08f0f7f
dNikon <denis.inikon@gmail.com>
2025-06-22 18:30:09 +0300

    Init extensions start app

--- src/app_factory.py
+++ src/app_factory.py
@@ -1,3 +1,7 @@
+import logging
+import time
+
+from config import d_niko_config
 from src.dniko_app import DNikoApp
 
 
@@ -8,4 +12,30 @@ def create_fast_api_app():
 
 def create_app() -> DNikoApp:
     app = create_fast_api_app()
+    initialize_extensions(app)
     return app
+
+
+def initialize_extensions(app: DNikoApp):
+    from extensions import (
+        ext_blueprints,
+    )
+
+    extensions = [
+        ext_blueprints,
+    ]
+    for ext in extensions:
+        short_name = ext.__name__.split(".")[-1]
+        is_enabled = ext.is_enabled() if hasattr(ext, "is_enabled") else True
+        if not is_enabled:
+            if d_niko_config.DEBUG:
+                logging.info(f"Skipped {short_name}")
+            continue
+
+        start_time = time.perf_counter()
+        ext.init_app(app)
+        end_time = time.perf_counter()
+        if d_niko_config.DEBUG:
+            logging.info(
+                f"Loaded {short_name} ({round((end_time - start_time) * 1000, 2)} ms)"
+            )
