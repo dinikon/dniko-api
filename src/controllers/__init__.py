af5ce3f2f820029835e20212b25017037bf4f054
dinikon <124208718+dinikon@users.noreply.github.com>
2025-06-28 16:32:35 +0300

    Add version prefix to API

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -1,12 +1,14 @@
 from fastapi import APIRouter
 
+from .constants import API_VERSION
+
 from .ui import router as ui_router
 from .vendors import router as vendors_router
 from .public_api import router as public_api_router
 from .platform import router as platform_router
 from .system import router as system_router
 
-router = APIRouter()
+router = APIRouter(prefix=f"/api/{API_VERSION}")
 
 router.include_router(system_router)
 router.include_router(ui_router)
---
+++ src/controllers/constants.py
@@ -0,0 +1,2 @@
+API_VERSION = "v1"
+"""Current API version prefix."""
